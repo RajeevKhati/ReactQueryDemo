592199ca9a1720a66ad25a2be166d6c4f18e0f81
Rajeev Khati <rajeev.khati@nagarro.com>
2022-09-20 17:26:46 +0530

    moving code in custom hook

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,18 +1,13 @@
-import axios from "axios";
 import React from "react";
-import { useQuery } from "react-query";
+import useSuperHeroesData from "../hooks/useSuperHeroesData";
 
-const fetchSuperHeroes = () => axios.get("http://localhost:4000/superheroes");
 const RQSuperHeroes = () => {
   const onSuccess = (data) => console.log("data loaded successfully ", data);
   const onError = (error) => console.log("data load error ", error);
 
-  const { isLoading, data, isError, error, isFetching } = useQuery(
-    "super-heroes",
-    fetchSuperHeroes,
-    //select helps to transform response data. Here we have change array of objects to array to hero names.
-    //We can use same method to filter out some elements as well.
-    { onSuccess, onError, select:(data) => data?.data.map(hero => hero.name) }
+  const { isLoading, data, isError, error, isFetching } = useSuperHeroesData(
+    onSuccess,
+    onError
   );
   if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
---
+++ src/hooks/useSuperHeroesData.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+const fetchSuperHeroes = () => axios.get("http://localhost:4000/superheroes");
+
+const useSuperHeroesData = (onSuccess, onError) => {
+  return useQuery(
+    "super-heroes",
+    fetchSuperHeroes,
+    //select helps to transform response data. Here we have change array of objects to array to hero names.
+    //We can use same method to filter out some elements as well.
+    { onSuccess, onError, select:(data) => data?.data.map(hero => hero.name) }
+  );
+}
+
+export default useSuperHeroesData
